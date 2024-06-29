ba54b92a2753ba5e29ebe237131cd9bb18ed4c35
Pushp Raj <pushprajsunshin@gmail.com>
2024-06-29 12:31:04 +0530

    datatypes

---
+++ 2.datatypes_EcmaStandards.js
@@ -0,0 +1,28 @@
+let name = "Pushp"
+let age = 20
+let issLoggedIn = true
+let city
+
+
+// number   => range-  2 to power 53
+// bigint
+// string   => ""
+// boolean  => true/false
+// null     => datatype as well as a standalone value
+// undefined
+// symbol    => unique
+// object
+
+
+console.log(typeof name);   // string
+
+console.log(typeof age);    //number
+
+console.log(typeof issLoggedIn);    //boolean
+
+console.log(typeof city);   // undefined
+
+console.log(typeof null);   // => This will not return "null" but returns "object" as datatype 🙄
+                            //null is itself a datatype but since it is also a value so the moment we check the type of null it returns "object" as datatype 🤡😄
+
+console.log(typeof undefined);  // returns "undefined"
